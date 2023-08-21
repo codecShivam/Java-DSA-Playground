31a03a4e8ff3895832f965699caddc1b0fbadf36
codecShivam <codecshivam@gmail.com>
2023-08-21 16:56:22 +0530

    primitives

--- src/com/shivam/Primitives.java
+++ src/com/shivam/Primitives.java
@@ -1,7 +1,13 @@
 package com.shivam;
 
-public class Primitives {
+public class Primitives { // primitive is any data that you cannot break into any other data type
     public static void main(String[] args) {
-
+    int rollNo = 64;
+   char letter = 'r'; //char starts with single colon
+    float marks = 98.67f;
+    double largeDecNum = 234578.43243f;
+    long largeInt = 3232323487473944L;
+    boolean check = false;
+    // String is not a primitive datatype
     }
 }
