906df4837678158d7a6b3d7a7e39fd1e1edb6950
codecShivam <codecshivam@gmail.com>
2023-08-21 17:24:53 +0530

    typecasting

---
+++ src/com/shivam/TypeCasting.java
@@ -0,0 +1,17 @@
+package com.shivam;
+
+import java.util.Scanner;
+
+public class TypeCasting {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+//        float num = input.nextFloat();
+//      int num = input.nextInt();
+//        System.out.println(num);
+
+//        type casting
+//        int num = (int)(67.56f);
+        float num = 33.6f;
+        System.out.println(num);
+    }
+}
