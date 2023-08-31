54e6f4e02b22c88011279a8b7a8937f14ed54312
codecShivam <codecshivam@gmail.com>
2023-09-01 01:25:56 +0530

    methods,scope,overloading,shadowing

---
+++ src/com/shivam/Methods.java
@@ -0,0 +1,13 @@
+package com.shivam;
+
+import java.util.Scanner;
+
+public class Methods {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int num1 = in.nextInt();
+        int num2 = in.nextInt();
+
+
+    }
+}
